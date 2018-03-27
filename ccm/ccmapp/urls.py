747d7c3ed283e41944123ec5c8bd407b4bc23a23
ChunYiLanShan <deepdeeplearning@163.com>
2018-03-26 18:36:03 -0700

    Add company parameter for project object

--- ccm/ccmapp/urls.py
+++ ccm/ccmapp/urls.py
@@ -6,5 +6,6 @@ from ccm.ccmapp import views
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'api/projects', views.ProjectViewSet, base_name='project')
+router.register(r'api/company', views.CompanyViewSet, base_name='company')
 
 urlpatterns = router.urls
